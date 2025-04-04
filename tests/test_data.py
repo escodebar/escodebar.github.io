1cd591823dafbbaf1b51cdadd583f6a71a36ea90
Pablo Escodebar <escodebar@gmail.com>
2025-04-04 11:09:58 +0200

    Refactor stack completeness test

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,19 +3,25 @@ import json
 import pytest
 
 
+COMPANIES = ["4tw", "adsy", "dectris", "hot"]
+
+
+def read_json_file(path):
+    with open(path) as fp:
+        return json.load(fp)
+
+
 @pytest.fixture
 def stack():
-    with open("assets/data/stack.json") as fp:
-        return json.load(fp)
+    return read_json_file("assets/data/stack.json")
 
 
-@pytest.fixture(params=["4tw", "adsy", "dectris", "hot"])
-def company(request):
-    with open(f"assets/data/assignments/{request.param}.json") as fp:
-        return json.load(fp)
+@pytest.fixture(params=COMPANIES)
+def assignments(request):
+    return read_json_file(f"assets/data/assignments/{request.param}.json")
 
 
-def test_the_stack_is_complete(stack, company):
+def test_the_stack_is_complete(stack, assignments):
     stack_keys = set(map(lambda x: x["key"], stack))
-    used_stack = set(reduce(lambda x, y: x + y["stack"], company, []))
+    used_stack = set(reduce(lambda x, y: x + y["stack"], assignments, []))
     assert used_stack.issubset(stack_keys), used_stack - stack_keys.intersection(used_stack)
