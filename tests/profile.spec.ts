04e20b2ec749da9f5d50a440921561c05b07280d
Pablo Escodebar <escodebar@gmail.com>
2025-04-24 16:24:54 +0200

    Move profile playwright test to step
    ...to further add steps to test highlighting functionality.

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("loads the homepage", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle(/Pablo/);
+test("highlight role", async ({ page }) => {
+  await test.step("loads the homepage", async () => {
+    await page.goto("/");
+    await expect(page).toHaveTitle(/Pablo/);
+  });
 });
