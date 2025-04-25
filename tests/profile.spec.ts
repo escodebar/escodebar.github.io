5450987e630f8d0ccf633b7535a83f27d08a3eb2
Pablo Escodebar <escodebar@gmail.com>
2025-04-25 08:01:37 +0200

    Test highlighting of assignments by role

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -6,7 +6,15 @@ test("highlight projects based on role", async ({ page }) => {
     await expect(page).toHaveTitle(/Pablo/);
   });
 
+  await expect(page.locator(".assignments .highlight")).toHaveCount(0);
+
   await test.step("clicking role", async () => {
     await page.getByRole("button", { name: "Software Engineer" }).click();
   });
+
+  for (const item of await page
+    .locator('.assignments [data-roles~="dev"]')
+    .all()) {
+    await expect(item).toHaveClass(/(^|\s)highlight(\s|$)/);
+  }
 });
